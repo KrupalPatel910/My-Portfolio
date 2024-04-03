e5462238dc20008c8b727b6e9ee28b12bc85b71d
Krupal Patel <krupal9102001@gmail.com>
2024-04-03 22:08:54 +0530

    Complete First Page

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// const lenis = new Lenis()
+
+// lenis.on('scroll', (e) => {
+//   console.log(e)
+// })
+
+// function raf(time) {
+//   lenis.raf(time)
+//   requestAnimationFrame(raf)
+// }
+
+// requestAnimationFrame(raf)
+
+
+
 window.addEventListener('scroll',function(){
     var navbar = document.querySelector('#navbar');
     navbar.classList.toggle('logo',window.scrollY>0);
